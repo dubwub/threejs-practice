c8051075aa4688821b761bed52b41763d22a11bc
Darwin Ding <dingdarwin@gmail.com>
2023-08-13 18:43:00 -0400

    nice progress. box onclick, learned a bit about cameras

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 // import { createRoot } from 'react-dom/client'
 import React from 'react';
 import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 
 function RotatingBox() {
   const myMesh = React.useRef<MeshProps>()
   useFrame(({ clock }: any) => {
       myMesh.current!.rotation.x = clock.getElapsedTime()
   })
+  const [ largeness, setLargeness ] = React.useState(2);
+  const accumulateAdditionalMass = () => {
+    // alert('whee!!');
+    setLargeness(largeness + 1);
+  }
   return (
-    <mesh ref={myMesh}>
-      <boxGeometry args={[2, 2, 2]} />
+    <mesh ref={myMesh} onClick={accumulateAdditionalMass}>
+      <boxGeometry args={[largeness, largeness, largeness]} />
       <meshStandardMaterial />
     </mesh>
   )
@@ -18,7 +24,9 @@ function RotatingBox() {
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }}>
+        <OrbitControls />
+        <axesHelper />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <RotatingBox />
